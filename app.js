f0d8e147fd69bcbe5c5c12118c6f83cac6ec6ca1
tango1989 <jwarren2000@verizon.net>
2023-05-09 11:14:39 -0700

    need to add text

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
+// hamburger
 const linkList = document.querySelector('.link-list');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
+openMenu.addEventListener('click', () =>{
+    linkList.show(linkList.style.display = 'flex', linkList.style.top = '0')
+});
+closeMenu.addEventListener('click', () => {
+    linkList.close(linkList.style.top = '-100%')
+});
 
-function show(){
-    linkList.style.display = 'flex';
-    linkList.style.top = '0';
-}
-function close(){
-    linkList.style.top = '-100%'
-}
+
+
+// form dialog modal 
+
+const openModal = document.querySelector('[data-open-modal]');
+const closeModal = document.querySelector('[data-close-modal]');
+const modal = document.querySelector('[data-modal]');
+
+openModal.addEventListener('click', () =>{
+    modal.showModal(modal.style.visibility = 'visible')
+});
+closeModal.addEventListener('click', () =>{
+    modal.close(modal.style.visibility = 'hidden')
+});
+
+// close modal outside modal box
+
+modal.addEventListener("click", e => {
+    const dialogDimensions = modal.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      modal.close()
+    }
+  });

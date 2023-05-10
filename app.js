4f53efa3c117d2af27cc889e96098c6d7710e357
tango1989 <jwarren2000@verizon.net>
2023-05-10 13:23:15 -0700

    Needs update only

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
 // hamburger
-const linkList = document.querySelector('.link-list');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+const linkList = document.querySelector(".link-list");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
 
-
-openMenu.addEventListener('click', () =>{
-    linkList.show(linkList.style.display = 'flex', linkList.style.top = '0')
+openMenu.addEventListener("click", () => {
+  linkList.show((linkList.style.display = "flex"), (linkList.style.top = "0"));
 });
-closeMenu.addEventListener('click', () => {
-    linkList.close(linkList.style.top = '-125%')
+closeMenu.addEventListener("click", () => {
+  linkList.close((linkList.style.top = "-125%"));
 });
 
+// form dialog modal
 
+const openModal = document.querySelector("[data-open-modal]");
+const closeModal = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
 
-// form dialog modal 
-
-const openModal = document.querySelector('[data-open-modal]');
-const closeModal = document.querySelector('[data-close-modal]');
-const modal = document.querySelector('[data-modal]');
-
-openModal.addEventListener('click', () =>{
-    modal.showModal(modal.style.visibility = 'visible')
+openModal.addEventListener("click", () => {
+  modal.showModal((modal.style.visibility = "visible"));
 });
-closeModal.addEventListener('click', () =>{
-    modal.close(modal.style.visibility = 'hidden')
+closeModal.addEventListener("click", () => {
+  modal.close((modal.style.visibility = "hidden"));
 });
 
 // close modal outside modal box
 
-modal.addEventListener("click", e => {
-    const dialogDimensions = modal.getBoundingClientRect()
-    if (
-      e.clientX < dialogDimensions.left ||
-      e.clientX > dialogDimensions.right ||
-      e.clientY < dialogDimensions.top ||
-      e.clientY > dialogDimensions.bottom
-    ) {
-      modal.close()
-    }
-  });
+modal.addEventListener("click", (e) => {
+  const dialogDimensions = modal.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    modal.close();
+  }
+});

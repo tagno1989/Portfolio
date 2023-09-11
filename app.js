65703dbabfc5db7950b1984f05628606c12bc5d9
tango1989 <jwarren2000@verizon.net>
2023-09-11 13:54:05 -0700

    style updates and form updates

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ openMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   linkList.close((linkList.style.top = "-125%"));
 });
+
 // hamburger close on selection of destination
 
 aboutClose.addEventListener("click", () => {
@@ -27,30 +28,66 @@ contactClose.addEventListener("click", () => {
 
 // form dialog modal
 
-// <!--!!!MODAL OVERRIDING BROWSERS ERROR MESSAGES!!! FIX?  -->
+const modal = document.querySelector("[data-modal]");
+const openModalBtn = document.querySelector("[data-open-modal]");
+const closeModalBtn = document.querySelector("[data-close-modal]");
+
+openModalBtn.addEventListener("click", () => {
+  if (form.checkValidity(true)){
+    modal.showModal((modal.style.visibility = "visible"));
+  } else {
+    alert('All Fields Required!')
+  };
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.close((modal.style.visibility = "hidden"));
+});
 
 
-// const openModal = document.querySelector("[data-open-modal]");
-// const closeModal = document.querySelector("[data-close-modal]");
-// const modal = document.querySelector("[data-modal]");
-
-// openModal.addEventListener("click", () => {
-//   modal.showModal((modal.style.visibility = "visible"));
-// });
-// closeModal.addEventListener("click", () => {
-//   modal.close((modal.style.visibility = "hidden"));
-// });
 
 // close modal outside modal box
 
-// modal.addEventListener("click", (e) => {
-//   const dialogDimensions = modal.getBoundingClientRect();
-//   if (
-//     e.clientX < dialogDimensions.left ||
-//     e.clientX > dialogDimensions.right ||
-//     e.clientY < dialogDimensions.top ||
-//     e.clientY > dialogDimensions.bottom
-//   ) {
-//     modal.close();
-//   }
-// });
+modal.addEventListener("click", (e) => {
+  const dialogDimensions = modal.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    modal.close();
+  }
+});
+
+// form validation 
+
+const email = document.getElementById('email');
+const nameX = document.getElementById('nameX');
+const form = document.getElementById('contact-form');
+const submit = document.getElementById('submit');
+
+
+
+email.addEventListener("input", (e) => {
+  if (email.validity.patternMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+    email.style.border = '2px dashed red';
+  } else {
+    email.setCustomValidity("");
+    email.style.border = '5px double #082f49'
+  }
+});
+
+nameX.addEventListener("input", (e) => {
+  if (nameX.validity.patternMismatch) {
+    nameX.setCustomValidity("I need your full name!");
+    nameX.style.border = '2px dashed red';
+  } else {
+    nameX.setCustomValidity("");
+    nameX.style.border = '5px double #082f49'
+  }
+});
+
+
+
